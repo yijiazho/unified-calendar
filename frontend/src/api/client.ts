3ec95ecd04517e4613c6e5143915bcfad824981f
yijiazho <tsihpemuh@gmail.com>
2026-06-13 11:29:33 -0700

    Impelemnt frontend auth login

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -6,4 +6,15 @@ const client = axios.create({
   withCredentials: true,
 })
 
+client.interceptors.response.use(
+  res => res,
+  err => {
+    const onAuthPage = ['/', '/login', '/signup'].includes(window.location.pathname)
+    if (err.response?.status === 401 && !onAuthPage) {
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
+  },
+)
+
 export default client
